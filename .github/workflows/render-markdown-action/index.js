58db9be2f34693af8b5701b3f9476e5e7ddb49bd
bjornherting <bjorn.k.herting@gmail.com>
2023-08-08 11:42:16 +0200

    Delete .github/workflows/render-markdown-action directory

--- .github/workflows/render-markdown-action/index.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-try {
-  const markdownFile = core.getInput('markdown-file');
-  console.log(`File name is ${markdownFile}!`);
-} catch (error) {
-  core.setFailed(error.message);
-}
